9f49443f78535ffd159f64cff328c082699a1294
Mahima136 <pjmahima136@gmail.com>
2025-09-13 21:09:47 +0530

    Add files via upload

---
+++ SumDivisibleBy3or5.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class SumDivisibleBy3or5{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int sum = 0;
+        for(int i = start; i <= end; i++){
+            if(i % 3 == 0 || i % 5 == 0){
+                sum +=i;            
+        }
+    }
+    System.out.println(sum);
+}
+}
+
