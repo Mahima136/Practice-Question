30cc62e9b31a0684b19f7c6dcfa530af742e39f4
Mahima136 <pjmahima136@gmail.com>
2025-10-24 20:15:19 +0530

    Add files via upload

---
+++ ASCIIValue.java
@@ -0,0 +1,10 @@
+public class ASCIIValue{
+    public static void main(String[]args){
+        String str = "Hi";
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            System.out.println(ch + " : " + (int) ch);
+        }
+    }
+}
+        
