9250bba46301c3e4b2e6f0f38f12e0bd399366d1
Mahima136 <pjmahima136@gmail.com>
2025-10-24 20:21:41 +0530

    Add files via upload

---
+++ UppercaseStr.java
@@ -0,0 +1,11 @@
+public class UppercaseStr{
+    public static void main(String[]args){
+        String str = "HeLlo WorLD";
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(Character.isUpperCase(ch)){
+                System.out.print(ch);
+            }
+        }
+    }
+}
